0d107270055046238897c4c27023d29f883ec94e
Kevin Su <kevinsu@outlook.co.nz>
2021-11-17 12:46:25 +1300

    forwarding

--- src/Components/Decorator.js
+++ src/Components/Decorator.js
@@ -20,22 +20,36 @@ function Decorator(props) {
     // it and adds extra functionality
 
     //example 2
+    // we can do this with func.call()
+    // so now our log data works with 
+    // all sort of work that needs to be logged
+    // instead of JUSt the data details function
     function dataDetails(data){
         return "Data logged: " + data;
     }
 
-    function logData(data, date){
-        return dataDetails(data) + " | date logged: " + date;
+    function logData(func, data, date){
+        let result = func.call(...arguments) + " | date logged: " + date;
+        return result;
     }
 
-    let data1 = logData("8 hour work", "Tuesday 19th Nov");
+    let data1 = logData(dataDetails, "8 hour work", "Tuesday 19th Nov");
 
+    // e.g. we can have another function
+    function dataDetails2(data){
+        return "Area worked: " + data;
+    }
+
+    // here we pass in the dataDetails2 function
+    let data2 = logData(dataDetails2, "Warehouse", "Monday");
+    
     return (
         <div>
             <p>pepe</p>
             <h1>Hello</h1>
             <p>{greeting1}</p>
             <p>{data1}</p>
+            <p>{data2}</p>
         </div>
     );
 }
